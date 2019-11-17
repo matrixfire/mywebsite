2f7b7f98c10e1d692dcf2a2a19bb7f2e2edc1856
bill <349500371@qq.com>
2019-11-17 16:10:52 +0800

    get object or 404

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
@@ -22,7 +22,8 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
 	'''...'''
-	topic = Topic.objects.get(id=topic_id)
+	# topic = Topic.objects.get(id=topic_id)
+	topic = get_object_or_404(Topic, id=topic_id)
 	# ...
 	if topic.owner != request.user:
 		raise Http404
